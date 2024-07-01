b5cce04e3f854e2a3ba083a8be1bdc7ec4396f1d
email.ankitapatel@gmail.com <email.ankitapatel@gmail.com>
2024-07-01 14:56:42 +0100

    mob version header_social & scroll_down

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -15,9 +15,9 @@ const Topbar = () => {
     <nav>
       <a href="#home" onClick={()=> setActiveNav('#home')} className={activeNav === '#home' ? 'active' : ''}><AiOutlineHome /></a>
       <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
-      <a href="#portfolio" onClick={()=> setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineFundProjectionScreen /></a>
-      <a href="#experience" onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><AiOutlineReconciliation /></a>
       <a href="#education" onClick={()=> setActiveNav('#education')} className={activeNav === '#education' ? 'active' : ''}><AiOutlineRead /></a>
+      <a href="#experience" onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><AiOutlineReconciliation /></a>
+      <a href="#portfolio" onClick={()=> setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineFundProjectionScreen /></a>    
       <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlineMessage /></a>
     </nav>
   )
